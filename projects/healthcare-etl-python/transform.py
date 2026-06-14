4691bf5c0f17c73e134616c6d50fc073fc23e777
Sai Sandeep <84892796+Ksaisandeepkumar@users.noreply.github.com>
2026-06-14 00:00:43 -0500

    Update transform.py

--- projects/healthcare-etl-python/transform.py
+++ projects/healthcare-etl-python/transform.py
@@ -1,11 +1,18 @@
-# transform.py
+import csv
 
-patients = [
-    {"patient_id": 1, "name": "Sai", "age": 24},
-    {"patient_id": 2, "name": "John", "age": 45}
-]
+patients = []
 
-for patient in patients:
-    patient["age_group"] = "Adult"
+with open("sample_patients.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        age = int(row["age"])
+
+        if age >= 65:
+            row["age_group"] = "Senior"
+        else:
+            row["age_group"] = "Adult"
+
+        patients.append(row)
 
 print(patients)
